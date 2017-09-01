736d242be883615b33befb7cec0a50176e59d411
padmavathi <yenugupadmavathi@gmail.com>
2017-09-01 19:41:08 +0530

    added to simple output statement

---
+++ Smallproject/src/dome/com/Smalldata.java
@@ -0,0 +1,27 @@
+package dome.com;
+
+public class Smalldata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		displayData();
+		
+		Smalldata sd=new Smalldata();
+		sd.display();
+		
+
+	}
+
+	private static void displayData() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		
+		
+	}
+	public void display()
+	{
+		System.out.println("HI");
+		System.out.println("How r u");
+	}
+
+}
